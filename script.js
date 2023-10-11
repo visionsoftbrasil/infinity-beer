64f892557f78c6e8c0510e174b0cd5c8e9bd7719
Vision Tech Software <ghjlsoftware@gmail.com>
2023-10-11 13:01:51 +0000

    style carousel

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 const slides = document.getElementById('container-slides')
-const button = document.getElementById('next')
-let dist = 464
+const next = document.getElementById('next')
+const prev = document.getElementById('prev')
+
+let dist = -464
 let counter = 1
 
-button.addEventListener('click', () => {
-
+next.addEventListener('click', () => {
+    console.log("PREV NEXT:", dist)
     if (counter >= slides.children.length) {
         slides.style.transform = `translateX(0px)`
-        dist = 464
+        dist = -464
         counter = 1
+        console.log("FINAL NEXT:", dist)
         return
     }
 
-    slides.style.transform = `translateX(-${dist}px)`
-    dist+=464
+    slides.style.transform = `translateX(${dist}px)`
+    dist-=464
     counter += 1
+    console.log("FINAL NEXT:", dist)
+})
+
+prev.addEventListener('click', () => {
+    console.log("PREV PREV:", dist)
+    if (counter == 1) {
+        dist*=3
+        slides.style.transform = `translateX(${dist}px)`
+        counter = 4
+        console.log("FINAL PREV:", dist)
+        return
+    }
+    dist+=464
+    slides.style.transform = `translateX(${dist}px)`
+    counter -= 1
+    console.log("FINAL PREV:", dist)
 })
